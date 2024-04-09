8da957be645171b53a85da9b72ed58cce3cbf93d
Giammi <giammarcoferranti@MacBook-Air-di-Giammi.local>
2024-04-09 11:42:53 +0200

    aggiunto input

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import axios from "axios";
+import { useState } from "react";
+
 function App() {
-  return <>Hello World</>;
+  const [img, setImg] = useState(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log(URL.createObjectURL(e.target[0].value));
+    setImg(new Blob(Array.from(e.target[0].value), { type: "image/jpeg" }));
+    //const res = axios.post("Url")
+  };
+  console.log(img);
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="file" />
+        <button type="submit">send</button>
+      </form>
+      {img ? <img src={"data:image/png;base64," + img} /> : null}
+    </>
+  );
 }
 
 export default App;
