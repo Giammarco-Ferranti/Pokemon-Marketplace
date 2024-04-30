7b0d8086a9c60ce5f2ef339aee9500f7d08e2f75
Giammi <giammarcoferranti@MacBook-Air-di-Giammi.local>
2024-04-30 16:20:29 +0200

    navbar design

--- client/src/Layouts/Hero.jsx
+++ client/src/Layouts/Hero.jsx
@@ -4,7 +4,7 @@ import gif from "../assets/blastoise-mega.gif";
 
 const Hero = () => {
   return (
-    <div className="flex flex-row py-16 w-full h-fit sm:h-screen bg-blue-100 justify-center items-center rounded-xl mt-5">
+    <div className="flex flex-row py-16 w-full h-fit sm:h-screen border border-gray-100 justify-center items-center rounded-3xl mt-5">
       <div className="w-full px-8 flex flex-col gap-10 items-center justify-center h-fit sm:flex-row  sm:gap-24">
         <div className="bg-gray-50 p-10 rounded-xl">
           <img
@@ -14,8 +14,10 @@ const Hero = () => {
           />
         </div>
         <div className="flex flex-col items-center justify-center gap-3  text-center sm:items-start sm:text-left">
-          <h3 className="text-4xl font-semibold">Gotta catch 'em all.</h3>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          <h3 className="text-4xl font-bold">Gotta catch 'em all.</h3>
+          <p className="font-semibold text-gray-600">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          </p>
           <Button className="">Explore</Button>
         </div>
         {/* <h1 className="text-4xl font-bold">Gotta Catch 'Em All.</h1>
--- client/src/Layouts/Navbar.jsx
+++ client/src/Layouts/Navbar.jsx
@@ -3,41 +3,29 @@ import { Input } from "@/components/ui/input";
 import pokemonSvg from "../assets/15.svg";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
+import avatar from "../assets/avatar.jpg";
 
 const NavbarLayout = () => {
   const navigate = useNavigate();
   const [token] = useState(localStorage.getItem("token"));
 
   return (
-    <div className="z-10 flex flex-row w-full justify-between h-10 items-center">
+    <div className="z-10 flex flex-row justify-between items-center w-full max-w-screen-xl h-fit bg-white py-4 px-4 border border-gray-100 rounded-full fixed bg-white/20 backdrop-blur-md">
       <div
         onClick={() => navigate("/")}
         className="flex flex-col items-center justify-center cursor-pointer"
       >
-        <img src={pokemonSvg} alt="pokemon-logo" className="w-20" />
+        <img src={pokemonSvg} alt="pokemon-logo" className="w-10" />
       </div>
       <div className="flex flex-row gap-5 items-center">
-        <Input type={"text"} placeholder="Search" />
+        <Input type={"text"} placeholder="Search Cards" />
 
         {token ? (
           <Button
             onClick={() => navigate("/profile")}
-            className="text-current bg-transparent hover:bg-gray-100 rounded-xl p-2"
+            className="text-current bg-transparent hover:bg-gray-100 rounded-2xl p-2"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-              />
-            </svg>
+            <img src={avatar} alt="avatar-img" className="w-12 rounded-full" />
           </Button>
         ) : (
           <Button onClick={() => navigate("/login")}>Login</Button>
