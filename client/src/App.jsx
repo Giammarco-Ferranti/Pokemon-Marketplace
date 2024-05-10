0873bb4b1c616c5f6beb0820184961b721ad29b6
Giammi <giammarcoferranti@MacBook-Air-di-Giammi.local>
2024-05-10 22:53:53 +0200

    removed packages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import AuthProvider from "./utils/Auth/AuthContext";
 import Routes from "./Routes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "./components/ui/sonner";
+import { Toaster } from "sonner";
 
 const queryClient = new QueryClient();
 
