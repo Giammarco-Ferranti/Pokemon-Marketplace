a0cfdd10edbf106473d287e21c63e49b93579362
Giammi <giammarcoferranti@MacBook-Air-di-Giammi.local>
2024-05-16 10:24:24 +0200

    build

--- api/db/connection.js
+++ api/db/connection.js
@@ -3,11 +3,7 @@ import "dotenv/config";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: "",
-  port: process.env.DB_PORT,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 export default pool;
