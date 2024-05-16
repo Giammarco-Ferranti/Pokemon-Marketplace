e1d42095eccde1ff1e36a696814528d34b6253f9
Giammi <giammarcoferranti@MacBook-Air-di-Giammi.local>
2024-05-16 11:33:40 +0200

    vercel

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const fetchData = async (method, url, data) => {
   const res = await axios({
     method: method,
-    url: `pokemon-marketplace-api.vercel.app${url}`,
+    url: `${import.meta.env.VITE_BASE_URL}${url}`,
     data: data,
   });
   return res.data;
