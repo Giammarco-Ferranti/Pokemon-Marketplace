ce4166c9142628adf887336987160f6a79893fb2
Giammi <giammarcoferranti@MacBook-Air-di-Giammi.local>
2024-05-16 10:59:15 +0200

    changed bcrypt

--- api/controllers/user/login.js
+++ api/controllers/user/login.js
@@ -1,5 +1,5 @@
 import pool from "../../db/connection.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 
--- api/controllers/user/signUp.js
+++ api/controllers/user/signUp.js
@@ -1,5 +1,5 @@
 import pool from "../../db/connection.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { v4 as uuidv4 } from "uuid";
 import { filterRequest } from "../../utils/filterRequest.js";
