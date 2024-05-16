ba2b0f0ddca02dea8c71a9b53b36c58b0958d3e4
Giammi <giammarcoferranti@MacBook-Air-di-Giammi.local>
2024-05-16 11:39:25 +0200

    vercel

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const fetchData = async (method, url, data) => {
   const res = await axios({
     method: method,
-    url: `${import.meta.env.VITE_BASE_URL}${url}`,
+    url: `https://${import.meta.env.VITE_BASE_URL}${url}`,
     data: data,
   });
   return res.data;
