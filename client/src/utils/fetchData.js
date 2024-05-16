799a01b9d4ba26d4a9fa146f74787261ee2c9f15
Giammi <giammarcoferranti@MacBook-Air-di-Giammi.local>
2024-05-16 11:24:29 +0200

    removed env from client

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const fetchData = async (method, url, data) => {
   const res = await axios({
     method: method,
-    url: `${import.meta.env.VITE_BASE_URL}${url}`,
+    url: `pokemon-marketplace-api.vercel.app${url}`,
     data: data,
   });
   return res.data;
